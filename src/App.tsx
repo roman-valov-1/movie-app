a6944327b056736a1af8201fb4cc485063d5bd8d
Roman <romandgo09@gmail.com>
2024-06-27 23:05:16 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'react-bootstrap'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Button>sad</Button>
+    </div>
+  )
+}
+
+export default App
