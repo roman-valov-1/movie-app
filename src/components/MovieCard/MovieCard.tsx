657284cda5b75aecaeea14360fd172371e1b7998
Roman <romandgo09@gmail.com>
2024-07-07 23:31:16 +0300

    keep working on UI

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styles from './MovieCard.module.css';
 
 function MovieCard() {
@@ -7,11 +8,26 @@ function MovieCard() {
             Image
          </div>
          <div className={styles['movie-card__info']}>
-            <div className={styles['movie-card__info-item']}>Title Name</div>
-            <div className={styles['movie-card__info-item']}>Genre</div>
-            <div className={styles['movie-card__info-item']}>country</div>
-            <div className={styles['movie-card__info-item']}>description</div>
+            <div className={styles['movie-card__info-item']}>
+               <span className={styles['movie-card__info-title']}>Title Name:</span>
+               Some title
+            </div>
+            <div className={styles['movie-card__info-item']}>
+               <span className={styles['movie-card__info-title']}>Genre:</span>
+               Genre 1, Genre 2, Genre 3, Genre 4.
+            </div>
+            <div className={styles['movie-card__info-item']}>
+               <span className={styles['movie-card__info-title']}>Country:</span>
+               List of countries
+            </div>
+            <div className={styles['movie-card__info-item']}>
+               <span className={styles['movie-card__info-title']}>Description:</span>
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda laudantium molestias architecto harum suscipit. Laboriosam corporis maxime quidem laborum, aspernatur magni nobis magnam vero, cupiditate amet accusantium, voluptas officia illum?
+            </div>
          </div>
+         <NavLink to='/login' className={styles['movie-card__info-link']}>
+            More details
+         </NavLink>
       </div>
    )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,44 @@
-import { useRef } from 'react';
+
 import MovieCard from '../../components/MovieCard/MovieCard';
 import TextInput from '../../components/TextInput/TextInput';
 import styles from './Home.module.css';
 import Button from '../../components/Button/Button';
+import Checkbox from '../../components/Checkbox/Checkbox';
+import { SyntheticEvent } from 'react';
 
 function Home() {
+
+   function submitHandler(e: SyntheticEvent) {
+      e.preventDefault();
+   }
 
    return (
       <div className='container'>
          <section className={styles['home']}>
             <h1 className='h1'>Let's find something good</h1>
-            <form className={styles['home__search-block']}>
+            <form className={styles['home__search-block']} onSubmit={submitHandler}>
                <TextInput 
                   type={'text'} 
                   name={'search'}
                   placeholder={"Type a film's name"}
                />
-               <Button>Find</Button>
+               <Checkbox 
+                  value={'criminal'}
+                  name={'genre'}
+               />
+               <Checkbox 
+                  value={'horror'}
+                  name={'genre'}
+               />
+               <Checkbox 
+                  value={'comedy'}
+                  name={'genre'}
+               />
+               <Button type='submit'>Find</Button>
             </form>
-            <div>
+            <div className={styles['home__movie-list']}>
+               <MovieCard />
+               <MovieCard />
                <MovieCard />
                <MovieCard />
                <MovieCard />
